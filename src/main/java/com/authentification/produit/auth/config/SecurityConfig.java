10473a73195ddabdd8b3a9792f0faaf358453652
anouar <anouar.ab95@gmail.com>
2024-12-04 10:26:30 +0100

    genner README

--- src/main/java/com/authentification/produit/auth/config/SecurityConfig.java
+++ src/main/java/com/authentification/produit/auth/config/SecurityConfig.java
@@ -1,27 +1,33 @@
 package com.authentification.produit.auth.config;
 
-import lombok.RequiredArgsConstructor;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
-@Configuration
+
 @EnableWebSecurity
-@RequiredArgsConstructor
+@Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
-
     @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+
+        auth.inMemoryAuthentication().withUser("ADMIN").password("{noop}123").authorities("ROLE_ADMIN");
+    }
+
+
+      @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .csrf().disable()
                 .authorizeRequests()
                 // Routes publiques
-                .antMatchers("/api/auth/**", "/api/auth/register").permitAll()
+                .antMatchers("/api/auth/**").permitAll()
                 // Routes admin
                 .antMatchers(
                         "/api/admin/categories/**",
@@ -34,21 +40,6 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                         "/api/user/produits/**")
                 .hasAnyRole("USER", "ADMIN")
                 .anyRequest().authenticated()
-                .and()
-                .formLogin()
-                .loginProcessingUrl("/api/auth")
-                .usernameParameter("login")
-                .passwordParameter("password")
-                .successHandler((request, response, authentication) -> {
-                    response.setContentType("application/json");
-                    response.setStatus(200);
-                    response.getWriter().write("{\"message\": \"Authentification réussie\"}");
-                })
-                .failureHandler((request, response, exception) -> {
-                    response.setContentType("application/json");
-                    response.setStatus(401);
-                    response.getWriter().write("{\"error\": \"Échec de l'authentification\"}");
-                })
                 .and()
                 .sessionManagement()
                 .maximumSessions(1)
